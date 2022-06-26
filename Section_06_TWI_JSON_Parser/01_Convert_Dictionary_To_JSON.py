d80b37c310721cad72096de2cc10a022f86baca4
“rajatt95” <rajatvermaa95@gmail.com>
2022-06-27 01:38:26 +0530

    Initial commit

---
+++ Section_06_TWI_JSON_Parser/01_Convert_Dictionary_To_JSON.py
@@ -0,0 +1,29 @@
+# /**
+# * @author Rajat Verma
+# * https://www.linkedin.com/in/rajat-v-3b0685128/
+# * https://github.com/rajatt95
+# * https://rajatt95.github.io/
+# *
+# * Course: Step by Step Rest API Testing using Python + Pytest +Allure (https://www.udemy.com/course/api-testing-python/)
+# * Tutor: Testing World Infotech (https://www.udemy.com/user/technology-world/)
+# */
+#
+# /***************************************************/
+
+# Convert Dictionary to JSON
+
+# json -> Package coming with Python
+import json
+
+# Data to be written
+dictionary = {
+    "id": "04",
+    "name": "sunil",
+    "department": "HR"
+}
+
+# Serializing json
+json_object = json.dumps(dictionary, indent=4)
+# json_object = json.dumps(dictionary)
+print(json_object)
+
---
+++ Section_08_TWI_API_Testing_KickOff/03_GET_Print_ResponseHeaders.py
@@ -0,0 +1,25 @@
+# /**
+# * @author Rajat Verma
+# * https://www.linkedin.com/in/rajat-v-3b0685128/
+# * https://github.com/rajatt95
+# * https://rajatt95.github.io/
+# *
+# * Course: Step by Step Rest API Testing using Python + Pytest +Allure (https://www.udemy.com/course/api-testing-python/)
+# * Tutor: Testing World Infotech (https://www.udemy.com/user/technology-world/)
+# */
+#
+# /***************************************************/
+
+# HTTP method GET call using requests package
+
+# URL
+import requests
+
+url = "https://reqres.in/api/users?page=2"
+
+# Send GET Request
+response = requests.get(url)
+
+# Print the Response Headers
+print(response.headers)
+
