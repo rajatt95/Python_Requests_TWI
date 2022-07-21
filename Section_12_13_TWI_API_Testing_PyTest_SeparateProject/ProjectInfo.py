6d91a6ef50d427eb00c5841abfc6e136aacec1f3
“rajatt95” <rajatvermaa95@gmail.com>
2022-07-22 01:42:10 +0530

    UDEMY - TWI (API testing: Section-12 and 13 completed -> PyTest)

---
+++ Section_12_13_TWI_API_Testing_PyTest_SeparateProject/ProjectInfo.py
@@ -0,0 +1,20 @@
+# /**
+# * @author Rajat Verma
+# * https://www.linkedin.com/in/rajat-v-3b0685128/
+# * https://github.com/rajatt95
+# * https://rajatt95.github.io/
+# * https://github.com/stars/rajatt95/lists/udemy-testing-world-infotech
+# * https://github.com/rajatt95/Python_Requests_TWI
+# *
+# * Course: Step by Step Rest API Testing using Python + Pytest +Allure (https://www.udemy.com/course/api-testing-python/)
+# * Tutor: Testing World Infotech (https://www.udemy.com/user/technology-world/)
+# */
+#
+# /***************************************************/
+
+#
+# /**
+#  * Project name: Python_PyTest_TWI
+#  * https://github.com/rajatt95/Python_PyTest_TWI
+#  * About: Python | PyCharm | PyTest | Unit Testing framework | Verbose | Execution - Specific file/folder | Decorator - skip, skipif | Register Custom Marker | Test case - Status, Tagging/Grouping | Assertions | Fixtures - Levels (Test, Module)| Reporting
+# */
